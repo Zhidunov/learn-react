8f8c800b92e1f13a1b2f0b5dea08aaa8d3967059
Nikita Zhidunov <zhidunov@gmail.com>
2020-02-18 12:50:34 +0300

    По итогу 20 урока. Добавлен NavLink вместо обычных ссылок. Изменен стиль меню.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import styles from './Navigation.module.css';
+import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
     <nav className={styles.nav}>
       <div className={styles.item}>
-        <a href='/profile'>Профиль</a>
+        <NavLink to='/profile' activeClassName={styles.active}>Профиль</NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/dialogs'>Сообщения</a>
+        <NavLink to='/dialogs' activeClassName={styles.active}>Сообщения</NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/news'>Новости</a>
+        <NavLink to='/news' activeClassName={styles.active}>Новости</NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/music'>Музыка</a>
+        <NavLink to='/music' activeClassName={styles.active}>Музыка</NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/settings'>Настройки</a>
+        <NavLink to='/settings' activeClassName={styles.active}>Настройки</NavLink>
       </div>
     </nav>
   );
