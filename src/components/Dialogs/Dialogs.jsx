a4ec34e753610e1a625a357224f1a64d7a14efcc
Nikita Zhidunov <zhidunov@gmail.com>
2020-02-18 13:20:00 +0300

    По итогу 21 урока. Редактирование страницы Messages.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import styles from './Dialogs.module.css';
+import React from "react";
+import styles from "./Dialogs.module.css";
 
-
-function Dialogs(props){
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
+function Dialogs(props) {
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogs_items}>
+          <div className={styles.dialog}>Андрей</div>
+          <div className={styles.dialog}>Михаил</div>
+          <div className={styles.dialog}>Дмитрий</div>
+          <div className={styles.dialog}>Алексей</div>
+          <div className={styles.dialog}>Константин</div>
+      </div>
+      <div className={styles.dialogs_messages}>
+          <div className={styles.message}>Привет!</div>
+          <div className={styles.message}>Как дела?</div>
+          <div className={styles.message}>Как успехи?</div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
