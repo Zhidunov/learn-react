387172dbe848cdd1809a1633303931c97ceba5ff
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-02-26 04:36:29 +0300

    По итогу 26 урока.
    Рефакторинг компоненты "Dialogs". Знакомство со слоями UI и BLL.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 import styles from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-function DialogItem(props) {
-  return (
-    <div className={styles.dialog}>
-      <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
-    </div>
-  );
-}
-
-function Message(props) {
-  return <div className={styles.message}>{props.message}</div>;
-}
+import DialogItem from "./DialogItem/DialogItem.jsx";
+import Message from "./Message/Message.jsx";
 
 function Dialogs(props) {
   let dialogsData = [
@@ -23,10 +12,6 @@ function Dialogs(props) {
     { id: 5, name: "Константин" }
   ];
 
-  let dialogsElements = dialogsData.map(dialog => (
-    <DialogItem id={dialog.id} name={dialog.name} />
-  ));
-
   let messages = [
     { id: 1, message: "Привет" },
     { id: 2, message: "Как дела" },
@@ -34,6 +19,10 @@ function Dialogs(props) {
     { id: 4, message: "Ку" },
     { id: 5, message: "Дратуте" }
   ];
+
+  let dialogsElements = dialogsData.map(dialog => (
+    <DialogItem id={dialog.id} name={dialog.name} />
+  ));
 
   let messagesElements = messages.map(message => (
     <Message message={message.message} />
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,24 +3,25 @@ import styles from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
 function MyPosts() {
-
   let posts = [
-    {id: 1, message: 'Это мой первый пост', likesCount: 12},
-    {id: 2, message: 'Это мой второй пост', likesCount: 7},
-];
+    { id: 1, message: "Это мой первый пост", likesCount: 12 },
+    { id: 2, message: "Это мой второй пост", likesCount: 7 }
+  ];
 
-let postsElement = posts.map(p => (<Post message={p.message} likesCount={p.likesCount}/>));
+  let postsElement = posts.map(p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
   return (
     <div className={styles.postsBlock}>
       <div>
-          <textarea></textarea>
-          <div><button>Опубликовать</button>
-          <button>Удалить</button></div>
+        <textarea />
+        <div>
+          <button>Опубликовать</button>
+          <button>Удалить</button>
+        </div>
       </div>
-      <div className={styles.posts}>
-        {postsElement}
-      </div>
+      <div className={styles.posts}>{postsElement}</div>
     </div>
   );
 }
