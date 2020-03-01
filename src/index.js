ce42a2ae84784095828fe8852020a7cc2e16e916
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-03-01 07:48:59 +0300

    37 урок.
    Рефакторинг state как хранилище данных в store с методами и приватными данными. Инкапсуляция. Привязка контекста вызова (bind).

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import state, { obs } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateNewMessageText
-} from "./redux/state.js";
 
 function rerenderEntireTree(state) {
+  debugger;
   ReactDOM.render(
     <App
       state={state}
-      addPost={addPost}
-      updateNewPostText={updateNewPostText}
-      updateNewMessageText={updateNewMessageText}
-      addMessage={addMessage}
+      addPost={store.addPost.bind(store)}
+      updateNewPostText={store.updateNewPostText.bind(store)}
+      updateNewMessageText={store.updateNewMessageText.bind(store)}
+      addMessage={store.addMessage.bind(store)}
     />,
     document.getElementById("root")
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-obs(rerenderEntireTree);
+store.subscriber(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,67 +1,68 @@
-let rerenderEntireTree;
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Это мой первый пост", likesCount: 12 },
-      { id: 2, message: "Это мой второй пост", likesCount: 7 }
-    ],
-    newPostText: "Text"
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Это мой первый пост", likesCount: 12 },
+        { id: 2, message: "Это мой второй пост", likesCount: 7 }
+      ],
+      newPostText: "Text"
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Андрей" },
+        { id: 2, name: "Михаил" },
+        { id: 3, name: "Дмитрий" },
+        { id: 4, name: "Алексей" },
+        { id: 5, name: "Константин" }
+      ],
+      messages: [
+        { id: 1, message: "Привет" },
+        { id: 2, message: "Как дела" },
+        { id: 3, message: "Чем занимаешься?" },
+        { id: 4, message: "Ку" },
+        { id: 5, message: "Дратуте" }
+      ],
+      newMessageText: "Сообщение..."
+    }
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Андрей" },
-      { id: 2, name: "Михаил" },
-      { id: 3, name: "Дмитрий" },
-      { id: 4, name: "Алексей" },
-      { id: 5, name: "Константин" }
-    ],
-    messages: [
-      { id: 1, message: "Привет" },
-      { id: 2, message: "Как дела" },
-      { id: 3, message: "Чем занимаешься?" },
-      { id: 4, message: "Ку" },
-      { id: 5, message: "Дратуте" }
-    ],
-    newMessageText: "Сообщение..."
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log("State changed");
+  },
+  addPost() {
+    let post = {
+      id: this._state.profilePage.posts.length + 1,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+
+    this._state.profilePage.posts.push(post);
+    this._state.profilePage.newPostText = "";
+    store._callSubscriber(this._state);
+  },
+  updateNewPostText(newPostText) {
+    this._state.profilePage.newPostText = newPostText;
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    let message = {
+      id: this._state.dialogsPage.messages.length + 1,
+      message: this._state.dialogsPage.newMessageText
+    };
+    this._state.dialogsPage.messages.push(message);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newMessageText) {
+    this._state.dialogsPage.newMessageText = newMessageText;
+    this._callSubscriber(this._state);
+  },
+  subscriber(observer) {
+    this._callSubscriber = observer;
   }
 };
 
-export function addPost() {
-  let post = {
-    id: state.profilePage.posts.length + 1,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-
-  state.profilePage.posts.push(post);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-}
-
-export function updateNewPostText(newPostText) {
-  state.profilePage.newPostText = newPostText;
-  rerenderEntireTree(state);
-}
-
-export function addMessage() {
-  let message = {
-    id: state.dialogsPage.messages.length + 1,
-    message: state.dialogsPage.newMessageText
-  };
-
-  state.dialogsPage.messages.push(message);
-  state.dialogsPage.newMessageText = "";
-  rerenderEntireTree(state);
-}
-
-export function updateNewMessageText(newMessageText) {
-  state.dialogsPage.newMessageText = newMessageText;
-  rerenderEntireTree(state);
-}
-
-export function obs(observer) {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
