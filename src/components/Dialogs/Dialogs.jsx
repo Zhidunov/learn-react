17b416ac01ea19de64c7042d903482c9bf7ae7ac
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-03-08 05:01:07 +0300

    43 урок. Контейнерная компонента.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,29 +2,25 @@ import React from "react";
 import styles from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem.jsx";
 import Message from "./Message/Message.jsx";
-import {
-  addMessageActionCreator,
-  updateNewMessageActionCreator
-} from "./../../redux/dialogsReducer.js";
 
 function Dialogs(props) {
-  let dialogsElements = props.state.dialogs.map(d => (
+  let dialogsElements = props.dialogs.map(d => (
     <DialogItem id={d.id} name={d.name} />
   ));
 
-  let messagesElements = props.state.messages.map(m => (
+  let messagesElements = props.messages.map(m => (
     <Message id={m.id} message={m.message} />
   ));
 
   let newMessageText = React.createRef();
 
-  function addMessage() {
-    props.dispatch(addMessageActionCreator());
+  function onAddMessage() {
+    props.onAddMessage();
   }
 
   function onMessageChange() {
     let text = newMessageText.current.value;
-    props.dispatch(updateNewMessageActionCreator(text));
+    props.onMessageChange(text);
   }
 
   return (
@@ -36,14 +32,14 @@ function Dialogs(props) {
         <div>
           <textarea
             onChange={onMessageChange}
-            value={props.state.newMessageText}
+            value={props.newMessageText}
             ref={newMessageText}
             cols="30"
             rows="5"
           />
         </div>
         <div>
-          <button onClick={addMessage}>Отправить</button>
+          <button onClick={onAddMessage}>Отправить</button>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import "./index.css";
 import App from "./App";
 
 function rerenderEntireTree(state) {
-  debugger;
   ReactDOM.render(
     <App state={state} dispatch={store.dispatch.bind(store)} store={store} />,
     document.getElementById("root")
   );
 }
 
+debugger;
 rerenderEntireTree(store.getState());
 
 store.subscribe(() => {
