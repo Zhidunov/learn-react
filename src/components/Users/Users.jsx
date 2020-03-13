6ed49086899595e9e530589f70c76f15b1d081ee
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-03-13 08:02:15 +0300

    52 и 53 уроки. Классовые компоненты.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,49 +2,54 @@ import React from "react";
 import styles from "./Users.module.css";
 import * as axios from "axios";
 
-function Users(props) {
-  axios.get("https://x1p7q.sse.codesandbox.io/users").then(res => {
-    props.setUsers(res.data.users);
-  });
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios.get("https://x1p7q.sse.codesandbox.io/users").then(res => {
+      this.props.setUsers(res.data.users);
+    });
+  }
 
-  return (
-    <div>
-      {props.users.map(u => (
-        <div className={styles.usersPage}>
-          <div>
-            <img src={u.avatarURL} alt="Пикча" className={styles.usersAva} />
+  render() {
+    return (
+      <div>
+        {this.props.users.map(u => (
+          <div className={styles.usersPage}>
+            <div>
+              <img src={u.avatarURL} alt="Пикча" className={styles.usersAva} />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    this.props.onUnFollow(u.id);
+                  }}
+                >
+                  Удалить
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    this.props.onFollow(u.id);
+                  }}
+                >
+                  Добавить
+                </button>
+              )}
+            </div>
+            <div>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </div>
+            <div>
+              <div>{u.location.country}</div>
+              <div>{u.location.city}</div>
+            </div>
           </div>
-          <div>
-            {u.followed ? (
-              <button
-                onClick={() => {
-                  props.onUnFollow(u.id);
-                }}
-              >
-                Удалить
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  props.onFollow(u.id);
-                }}
-              >
-                Добавить
-              </button>
-            )}
-          </div>
-          <div>
-            <div>{u.name}</div>
-            <div>{u.status}</div>
-          </div>
-          <div>
-            <div>{u.location.country}</div>
-            <div>{u.location.city}</div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Users;
