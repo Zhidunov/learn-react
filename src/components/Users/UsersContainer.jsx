88306baafea32c4922a23a7f0d4bf1291729f7c3
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-04-23 00:01:40 +0300

    81 урок. Селекторы.
    Реализовал селекторы для части стейта с пользователями и зарефакторил страницу пользователей.

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,8 +4,16 @@ import {
   setToggleIsFollowing,
   getUsersTC,
   setUnFollowTC,
-  setFollowTC,
+  setFollowTC
 } from "./../../redux/usersReducer.js";
+import {
+  getUsers,
+  getPageSize,
+  getTotalUsersCount,
+  getCurrentPage,
+  getIsFetching,
+  getFollowingInProgress
+} from "./../../redux/usersSelectors.js";
 import { connect } from "react-redux";
 import Users from "./Users.jsx";
 import Preloader from "./../Common/Preloader/Preloader.jsx";
@@ -46,12 +54,12 @@ class UsersContainer extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress
+    users: getUsers(state),
+    pageSize: getPageSize(state),
+    totalUsersCount: getTotalUsersCount(state),
+    currentPage: getCurrentPage(state),
+    isFetching: getIsFetching(state),
+    followingInProgress: getFollowingInProgress(state)
   };
 }
 
