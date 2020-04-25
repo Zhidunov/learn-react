7f985b23ed58221410bf4163dc5efb5576a6e471
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-04-26 02:17:20 +0300

    84 урок. React-hooks: useState().

--- src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -2,20 +2,39 @@ import React, { useState } from "react";
 import styles from "./ProfileStatus.module.css";
 
 const ProfileStatusWithHooks = props => {
-  let stateWithSetState = useState(false);
-  let editMode = stateWithSetState[0];
-  let setEditMode = stateWithSetState[1];
+  let [editMode, setEditMode] = useState(false);
+  let [status, setStatus] = useState(props.status);
+
+  const activateEditMode = () => {
+    setEditMode(true);
+  };
+
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateStatusTC(status);
+  };
+
+  const onStatusChange = e => {
+    setStatus(e.currentTarget.value);
+  };
 
   return (
     <div>
       {!editMode && (
         <div>
-          <span>{props.status || "Emty status"}</span>
+          <span onDoubleClick={activateEditMode}>
+            {props.status || "Emty status"}
+          </span>
         </div>
       )}
       {editMode && (
         <div>
-          <input autoFocus={true} />
+          <input
+            onChange={onStatusChange}
+            autoFocus
+            onBlur={deactivateEditMode}
+            value={status}
+          />
         </div>
       )}
     </div>
