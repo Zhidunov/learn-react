cfe21b6776e4216723fab72c5353989428b5498a
Nikita Zhidunov <zhidunov@gmail.com>
2020-04-29 01:08:46 +0300

    91 урок. Добавил изменения в создание store для расширения ReactDevTools для хрома

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import profileReducer from "./profileReducer.js";
 import dialogsReducer from "./dialogsReducer.js";
 import sidebarReducer from "./sidebarReducer.js";
@@ -7,6 +7,7 @@ import authReducer from "./authReducer.js";
 import thunkMiddleware from "redux-thunk";
 import { reducer as formReducer } from "redux-form";
 import appReducer from "./appReducer.js";
+
 
 let reducers = combineReducers({
   profilePage: profileReducer,
@@ -18,6 +19,9 @@ let reducers = combineReducers({
   app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+window.__store__ = store;
 
 export default store;
